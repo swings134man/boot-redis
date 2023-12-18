75f5728f83a5d565d4e206c32a3e5013109da2d0
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-12-18 21:43:13 +0900

    Redis Cache - Error Handler ADD

--- src/main/java/com/boot/redis/config/RedisRepositoryConfig.java
+++ src/main/java/com/boot/redis/config/RedisRepositoryConfig.java
@@ -1,9 +1,12 @@
 package com.boot.redis.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.CacheKeyPrefix;
@@ -30,6 +33,7 @@ import java.time.Duration;
  * @version : 1.0.0
  * @Description : Lettuce 사용(비동기 요청 처리), RedisRepository 방식.
  ************/
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 @EnableRedisRepositories
@@ -37,6 +41,8 @@ import java.time.Duration;
 public class RedisRepositoryConfig {
 
     private final RedisProperties redisProperties;
+
+    private final RedisTemplate<String, Object> redisTemplate;
 
     // lettuce
     @Bean
@@ -66,6 +72,36 @@ public class RedisRepositoryConfig {
                 .entryTtl(Duration.ofMinutes(1)); // 캐시 수명 1분
         builder.cacheDefaults(configuration);
         return builder.build();
+    }
+
+    @Bean
+    public CacheErrorHandler errorHandle() {
+        CacheErrorHandler cacheErrorHandler = new CacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+                log.error("Redis Cache GET Error --key:" + key + " Exception information:" + exception);
+                // if Get Error -> Delete Cache Data
+                // This Code makes Cache Data to be reloaded.
+                // !!But Many Times need to reload!! -> So, change this code
+//                redisTemplate.delete(cache.getName() + "*");
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+                log.error("Redis Cache PUT Error --key:" + key + " Exception information:" + exception);
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+                log.error("Redis Cache EVICT Error --key:" + key + " Exception information:" + exception);
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException exception, Cache cache) {
+                log.error("Redis Cache CLEAR Error -- Exception information:" + exception);
+            }
+        };
+        return cacheErrorHandler;
     }
 
     /**
