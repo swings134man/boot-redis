4f9035400d86fbdfe3af4c7a83857d397db9411b
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-12-28 00:42:51 +0900

    Main 랜딩 페이지 ADD

---
+++ src/main/java/com/boot/redis/HomeController.java
@@ -0,0 +1,19 @@
+package com.boot.redis;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        log.info("call home -> main page /");
+        return "home";
+    }
+
+}
