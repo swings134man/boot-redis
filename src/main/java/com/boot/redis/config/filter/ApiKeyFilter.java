2c1ebf9f08e1aaf6f4407b2c6c986d15db9254aa
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-03-06 13:35:09 +0900

    [OpenAPI] OncePerRequestFilter ADD

--- src/main/java/com/boot/redis/config/filter/ApiKeyFilter.java
+++ src/main/java/com/boot/redis/config/filter/ApiKeyFilter.java
@@ -1,31 +1,86 @@
-//package com.boot.redis.config.filter;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//public class ApiKeyFilter extends OncePerRequestFilter {
-//
-//    private String API_KEY_HEADER = "Authentication";
-//
-//    private final String apiKey;
-//
-//    public ApiKeyFilter(String apiKey) {
-//        this.apiKey = apiKey;
-//    }
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, ServletException, IOException {
-//        String requestApiKey = request.getHeader(API_KEY_HEADER);
-//        if (apiKey.equals(requestApiKey)) {
-//            filterChain.doFilter(request, response);
-//        } else {
-//            response.sendError(HttpStatus.FORBIDDEN.value(), "Invalid API Key");
-//        }
-//    }
-//}
+package com.boot.redis.config.filter;
+
+import com.boot.redis.open.OpenApi;
+import com.boot.redis.open.service.OpenApiService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    private final String API_KEY_HEADER = "Authentication";
+
+    private OpenApiService openApiService;
+
+    public ApiKeyFilter(OpenApiService openApiService) {
+        this.openApiService = openApiService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("---------------------------------------- Once Filter Activated ----------------------------------------");
+
+        log.info("Once Filter Request = {}", request.getRequestURL());
+
+        try {
+            checkApiKey(request, response);
+
+            filterChain.doFilter(request, response);
+        }catch (Exception e) {
+            log.error("Error in Once Filter", e);
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            return;
+        }
+
+
+        log.info("---------------------------------------- Once Filter END ----------------------------------------");
+    }
+
+    private void checkApiKey(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        log.info("Filter START");
+
+        OpenApi openApi = null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+        log.info("HEADER:: {}", request.getHeader(API_KEY_HEADER));
+        String reqApiKey = request.getHeader(API_KEY_HEADER);
+
+        if(reqApiKey == null || reqApiKey.isEmpty()) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "API Key is missing");
+            return;
+        }else {
+            openApi = openApiService.getApiKey(reqApiKey);
+
+            if(openApi == null) {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid API Key");
+                return;
+            }
+
+            LocalDate exprDate = LocalDate.parse(openApi.getApiMngrExprDate(), formatter);
+            LocalDate today = LocalDate.now();
+
+            if(reqApiKey.equals(openApi.getApiKey()) ) {
+                if(today.isAfter(exprDate)) {
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN, "API Key is expired");
+                    return;
+                }
+
+
+            } else {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN, "Is not Correct API Key");
+            }
+        }
+
+
+        log.info("Filter END");
+    }
+}
--- src/main/java/com/boot/redis/config/filter/FilterConfig.java
+++ src/main/java/com/boot/redis/config/filter/FilterConfig.java
@@ -1,12 +1,22 @@
 package com.boot.redis.config.filter;
 
+import com.boot.redis.open.service.OpenApiService;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 public class FilterConfig implements WebMvcConfigurer {
+
+    private final OpenApiService openApiService;
+
+    public FilterConfig(OpenApiService openApiService) {
+        this.openApiService = openApiService;
+    }
 
     @Bean
     public FilterRegistrationBean<ApiKeyFilterTest> filterBean(ApiKeyFilterTest apiKeyFilterTest) {
@@ -14,6 +24,16 @@ public class FilterConfig implements WebMvcConfigurer {
         filterRegBean.setFilter(apiKeyFilterTest);
         filterRegBean.addUrlPatterns("/open/*");
 
+        return filterRegBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean apiFilter() {
+        FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
+        filterRegBean.setFilter(new ApiKeyFilter(openApiService));
+        List<String> urlPatterns = new ArrayList<>();
+        urlPatterns.add("/board/v1/*");
+        filterRegBean.setUrlPatterns(urlPatterns);
         return filterRegBean;
     }
 
