fa0d12efbbc8cccb5d8c35024b52bcd650e0fe46
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-03-30 20:22:22 +0900

    add: CORS Filter ADD
    - CORS Filter 추가. WebMvcConfigurer

--- src/main/java/com/boot/redis/config/filter/FilterConfig.java
+++ src/main/java/com/boot/redis/config/filter/FilterConfig.java
@@ -4,6 +4,9 @@ import com.boot.redis.open.service.OpenApiService;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
@@ -16,6 +19,25 @@ public class FilterConfig implements WebMvcConfigurer {
 
     public FilterConfig(OpenApiService openApiService) {
         this.openApiService = openApiService;
+    }
+
+    // for Test -> Allow *
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsConfig() {
+        CorsConfiguration config = new CorsConfiguration();
+            config.setAllowCredentials(false);
+            config.addAllowedOrigin("*");
+            config.addAllowedHeader("*");
+            config.addAllowedMethod("*");
+            config.setMaxAge(6000L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean(new CorsFilter(source));
+        filterBean.setOrder(0);
+
+        return filterBean;
     }
 
     @Bean
