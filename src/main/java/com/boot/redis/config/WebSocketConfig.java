a2af57d639ad7b076c2955136de711b2c0c9efd1
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2025-03-14 17:20:09 +0900

    WIP: WebSocket HeartBeat 설정

--- src/main/java/com/boot/redis/config/WebSocketConfig.java
+++ src/main/java/com/boot/redis/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.boot.redis.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,9 +13,18 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocket // webSocket
 @EnableWebSocketMessageBroker // STOMP
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    /**
+     * @info : STOMP 설정
+     * @param registry
+     * @description : heartBeat 의 경우 Client 에서 client.heartbeat.outgoing, client.heartbeat.incoming 설정 필요.
+     *
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub"); //msg 받을경로(구독)
+//                .setTaskScheduler(heartbeatScheduler())
+//                .setHeartbeatValue(new long[]{300000, 300000}); // heartBeat 5min(300s)
         registry.setApplicationDestinationPrefixes("/pub"); //msg 보낼경로(발행)
     }
 
@@ -22,4 +33,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-stomp").setAllowedOrigins("*").withSockJS();
         registry.addEndpoint("/ws-stomp").setAllowedOrigins("*");
     }
+
+
+    // Heartbeat scheduler
+//    @Bean
+//    public ThreadPoolTaskScheduler heartbeatScheduler() {
+//        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+//        scheduler.setPoolSize(1);
+//        scheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+//        scheduler.initialize();
+//        return scheduler;
+//    }
 }
