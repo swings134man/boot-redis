2a78f442bafd1c27b1f7572fb033b935ef52e6f0
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2025-03-24 17:38:43 +0900

    fix: Spin Lock Key 설정 오류 수정 및 max 재시도 횟수 증가

--- src/main/java/com/boot/redis/about_redis/lock/RedisLockService.java
+++ src/main/java/com/boot/redis/about_redis/lock/RedisLockService.java
@@ -14,7 +14,10 @@ import java.util.concurrent.TimeUnit;
  * @version : 1.0.0
  * @Description : Redis Lock 테스트 설정을 위한 Service 클래스.
  *
- * -- Lettuce Spin Lock 방식?
+ * -- Lettuce Spin Lock 방식
+ * - 1개의 Redis Node 의 단일책임 방식 -> 대규모 방식(N개 Node) 채용시 RedLock 사용 권장
+ *
+ * - 단일 node Redis 일 경우, 장애시 문제발생 + redis 성능 문제 발생(부하)
  ************/
 @Service
 @RequiredArgsConstructor
@@ -51,11 +54,11 @@ public class RedisLockService {
      */
     public boolean acquireLockRetry(String lockKey, String requestId, long expireTime) {
         int retryCount = 0;
-        int maxRetryCount = 10;
+        int maxRetryCount = 20;
         boolean isLock = false;
 
         while(!isLock && retryCount < maxRetryCount){
-            isLock = this.acquireLock(getLockKey(lockKey), requestId, expireTime);
+            isLock = this.acquireLock(lockKey, requestId, expireTime);
 
             if(!isLock) {
                 try {
@@ -72,7 +75,7 @@ public class RedisLockService {
     }
 
     /**
-     * 랜덤한 재시도 시간 반환 (100ms ~ 500ms)
+     * 랜덤한 재시도 시간 반환 (100ms ~ 500ms) - BackOff Time(Busy Waiting)
      */
     private long getRandomSleepTime() {
         return SPIN_INTERVAL_MIN + (long) (Math.random() * (SPIN_INTERVAL_MAX - SPIN_INTERVAL_MIN));
